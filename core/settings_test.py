c57e2add4a2e40350daf4d2f35d058719950e1d3
CADestuamante <2322004hongphuc@gmail.com>
2026-04-02 09:43:03 +0700

    Test CORS va JWT

---
+++ core/settings_test.py
@@ -0,0 +1,10 @@
+from .settings import *
+
+
+# Use an isolated SQLite database for automated tests.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'test_db.sqlite3',
+    }
+}
